a7cb35970cf9b1e289adb13aabf5297143477d6e
pragnyak <52558118+pragnyak@users.noreply.github.com>
2019-09-09 16:50:47 +0530

    Update admin.py

--- admin_app/admin.py
+++ admin_app/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from admin_app.models import Heart_Waitlist
-from admin_app.models import Heart_Donor
+from admin_app.models import Heart_Waitlist,Liver_Waitlist,Kidney_Waitlist
+from admin_app.models import Heart_Donor,Liver_Donor,Kidney_Donor
 admin.site.register(Heart_Waitlist)
 admin.site.register(Heart_Donor)
+admin.site.register(Liver_Waitlist)
+admin.site.register(Liver_Donor)
+admin.site.register(Kidney_Waitlist)
+admin.site.register(Kidney_Donor)
