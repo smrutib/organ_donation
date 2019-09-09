013d1d6450f6bff74e1233bcfab56eeebbdb3f14
pragnyak <52558118+pragnyak@users.noreply.github.com>
2019-09-09 16:45:11 +0530

    Update models.py

--- admin_app/models.py
+++ admin_app/models.py
@@ -47,3 +47,60 @@ class Heart_Donor(models.Model):
 		)
 	blood_type = models.CharField(max_length=3, choices=blood_choices)
 
+class Kidney_Waitlist(models.Model):
+	hos_name = models.CharField(max_length=100)
+	patient_id = models.CharField(max_length =5, primary_key=True)
+	p_name = models.CharField(max_length=75)
+	status_choices =(
+		('I','Inactive'),
+		('A','Active')
+		)
+	status = models.CharField( max_length=1 , choices=status_choices, default='A' )
+	categories=(
+		('MA','Male Adult'),
+		('FA','Female Adult'),
+		('C','Child'),)
+	category=models.CharField(max_length=2,choices=categories)
+	blood_choices =(
+		('A+','A+'),
+		('A-','A-'),
+		('B+','B+'),
+		('B-','B-'),
+		('AB+','AB+'),
+		('AB-','AB-'),
+		('O+','O+'),
+		('O-','O-'),
+		)
+	blood_type = models.CharField(max_length=3, choices=blood_choices)
+	tissue_type1=models.IntegerField()
+	tissue_type2=models.IntegerField()
+	tissue_type3=models.IntegerField()
+	tissue_type4=models.IntegerField()
+	tissue_type5=models.IntegerField()
+	tissue_type6=models.IntegerField()
+
+class Kidney_Donor(models.Model):
+	donor_id = models.CharField(max_length =5, primary_key=True)
+	d_name = models.CharField(max_length=75)
+	blood_choices =(
+		('A+','A+'),
+		('A-','A-'),
+		('B+','B+'),
+		('B-','B-'),
+		('AB+','AB+'),
+		('AB-','AB-'),
+		('O+','O+'),
+		('O-','O-'),
+		)
+	blood_type = models.CharField(max_length=3, choices=blood_choices)
+	categories=(
+		('MA','Male Adult'),
+		('FA','Female Adult'),
+		('C','Child'),)
+	category=models.CharField(max_length=2,choices=categories)
+	tissue_type1=models.IntegerField()
+	tissue_type2=models.IntegerField()
+	tissue_type3=models.IntegerField()
+	tissue_type4=models.IntegerField()
+	tissue_type5=models.IntegerField()
+	tissue_type6=models.IntegerField()
